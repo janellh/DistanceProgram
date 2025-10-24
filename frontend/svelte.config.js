c542f3451be83eebffd4b9e9878a63c73f921c88
JaNell Haynes <janell@greencompassglobal.com>
2025-10-24 13:59:55 -0600

    sync lkockfile for Render build

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,7 @@
 // svelte.config.js
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+//import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   preprocess: vitePreprocess(),
